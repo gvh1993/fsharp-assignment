7ef900daab8caeb149c9aa8c67a7537616ed4099
DESKTOP-7BMGD7J\gertj <gertjanvanh@live.nl>
2017-03-30 11:50:47 +0200

    got monogame working in F#

---
+++ Fsharp assignment/Game/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+
+open Simulation
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    use g = new Game1()
+    g.Run()
+    0 // return an integer exit code
